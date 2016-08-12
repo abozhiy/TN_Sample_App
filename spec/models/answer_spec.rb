ba6f1b4e2746484997875ecca557266f25aee2e0
Anton Bozhiy <abozhiy@gmail.com>
2016-08-12 12:49:18 +0300

    Изменил очередность написания тестов(сначала ассоциации, затем валидации)

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Answer, type: :model do
   let(:question) { create(:question) }
   let(:answer) { build(:answer, question: question) }
 
+  it { should belong_to :question }
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :body }
-  it { should belong_to :question }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Question, type: :model do
 
   let(:question) { create(:question) }
 
+  it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should have_many(:answers).dependent(:destroy) }
 end
