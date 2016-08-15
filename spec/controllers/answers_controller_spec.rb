84e4d15842d01772fede7db10e2a34c342b70688
Anton Bozhiy <abozhiy@gmail.com>
2016-08-15 13:44:31 +0300

    Добавил проверку, что созданный ответ связан с вопросом - answers_controller_spec:26, 38

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe AnswersController, type: :controller do
     context "create with valid attributes" do
 
       it "saves a new answer into the database" do
-        expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(question.answers, :count).by(1)
       end
 
       it "redirects to question path" do
@@ -35,7 +35,7 @@ RSpec.describe AnswersController, type: :controller do
     context "create with invalid attributes" do
 
       it "doesn't save a new answer into the database" do
-        expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
+        expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(question.answers, :count)
       end
 
       it "re-renders new view" do
