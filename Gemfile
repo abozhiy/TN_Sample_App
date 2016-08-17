1b156d6766e4c478ff130fa856f138c7782eab12
Anton Bozhiy <abozhiy@gmail.com>
2016-08-17 03:25:07 +0300

    добавил gem devise, написал первые asseptance тесты на User

--- Gemfile
+++ Gemfile
@@ -1,19 +1,13 @@
 source 'https://rubygems.org'
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
-# Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
-# Use Puma as the app server
 gem 'puma', '~> 3.0'
-# Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-# Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-# Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
-# See https://github.com/rails/execjs#readme for more supported runtimes
+gem 'devise'
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
@@ -42,6 +36,8 @@ end
 
 group :test do
   gem 'shoulda-matchers'
+  gem 'capybara'
+  gem 'launchy'
 end
 
 group :development do
---
+++ spec/acceptance/create_question_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature "Create question", %q{
+  In order to get answer from community
+  As an authenticated user
+  I whant to be able to ask question
+} do
+
+  scenario 'Authenticated user creates question' do
+    User.create!(email: "user@test.com", password: "12345678")
+
+    visit new_user_session_path
+    fill_in "Email", with: "user@test.com"
+    fill_in "Password", with: "12345678"
+    click_on "Sign in"
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in "Title", with: "Test question"
+    fill_in "Body", with: "text text"
+    click_on "Create"
+
+    expect(page).to have_content "Your question successfuly created."
+  end
+
+  scenario 'Non-authenticated user ties to create question' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content "You need to sign in or sign up before continuing."
+  end
+end
