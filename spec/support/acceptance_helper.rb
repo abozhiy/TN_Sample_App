4fbd307451e2e4690f7ed85ba2accfba1997a56a
Anton Bozhiy <abozhiy@gmail.com>
2016-08-19 16:29:47 +0300

    Покрыл тестами реализовал Sign out и Sign up истории

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -5,4 +5,12 @@ module AcceptanceHelper
     fill_in 'Password', with: user.password
     click_on 'Sign in'
   end
+
+  def sign_up(user)
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+  end
 end
