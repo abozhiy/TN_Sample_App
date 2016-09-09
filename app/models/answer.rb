eef511cf82f6364072da919d5d23838352a2f9ec
Anton Bozhiy <abozhiy@gmail.com>
2016-09-09 03:24:55 +0300

    Polymorphic association attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,10 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachable, dependent: :destroy
   validates :question_id, :user_id, :body, presence: true
+
+  accepts_nested_attributes_for :attachments
 
   default_scope { order(best: :desc, created_at: :asc) }
   scope :best, -> { where(best: true) }
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :attachments, dependent: :destroy, inverse_of: :question
+  has_many :attachments, as: :attachable, dependent: :destroy
   validates :title, :body, :user_id, presence: true
 
   accepts_nested_attributes_for :attachments
