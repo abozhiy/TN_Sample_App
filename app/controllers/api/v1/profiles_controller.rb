e2de9dd01330ad74ad71ece6b0c4128756f81785
Anton Bozhiy <abozhiy@gmail.com>
2016-10-13 19:44:17 +0300

    Добавил gem gem 'doorkeeper'

---
+++ app/controllers/api/v1/profiles_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::ProfilesController < ApplicationController
+  before_action :doorkeeper_authorize!
+
+  skip_authorization_check
+
+  respond_to :json
+
+  def me
+    respond_with current_resource_owner
+  end
+
+  def index
+    @users = User.where.not(id: current_resource_owner)
+    respond_with @users
+  end
+
+
+  protected
+
+    def current_resource_owner
+      @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
+end
--- db/migrate/20161006100158_add_confirm_email_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddConfirmEmailToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :confirmation_email, :string
-    add_column :users, :confirm_email, :boolean, default: false
-  end
-end
