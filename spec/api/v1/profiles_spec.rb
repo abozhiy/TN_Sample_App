fa60f904e90c52a637fca6971d04e24b270b5b10
Anton Bozhiy <abozhiy@gmail.com>
2016-10-14 18:45:42 +0300

    Правки согласно замечаниям

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -54,7 +54,7 @@ describe 'Profile API' do
 
     context 'authorized' do
       let(:me) { create(:user) }
-      let(:users) { create_list(:user, 3) }
+      let!(:users) { create_list(:user, 3) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
       before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
@@ -73,13 +73,13 @@ describe 'Profile API' do
 
       %w(id email created_at updated_at admin).each do |attr|
         it "contains #{attr}" do
-          expect(response.body).to be_json_eql(users.first.send(attr.to_sym).to_json).at_path(attr)
+          expect(response.body).to be_json_eql(users.first.send(attr.to_sym).to_json).at_path("0/#{attr}")
         end
       end
       
       %w(password encrypted_password).each do |attr|
         it "does not contain #{attr}" do
-          expect(response.body).to_not have_json_path(attr)
+          expect(response.body).to_not have_json_path("0/#{attr}")
         end
       end
     end
